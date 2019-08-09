d53a3f1a50ec5054da0ca7bc1e53a3b432beb6a5
Carter Brown <carterbrwn2@gmail.com>
2019-08-09 16:02:46 -0400

    Added postgresql and Accounts HelloWorld

--- ceti_data_commons/ceti_data_commons/settings.py
+++ ceti_data_commons/ceti_data_commons/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'ceti_data_commons.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cetidc',
+        'USER': 'postgres',
+        'PASSWORD': 'ctarter',
+        'HOST': 'localhost'
     }
 }
 
