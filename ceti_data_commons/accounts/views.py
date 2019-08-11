e1880eef733b6302282ad2f4189be551d59cddb3
carterbrwn2 <carterbrwn2@gmail.com>
2019-08-11 15:48:22 -0400

    Created basic bootstrap theme/layout.

--- ceti_data_commons/accounts/views.py
+++ ceti_data_commons/accounts/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 
 
-def index(request):
-    return render(request, 'accounts/index.html')
+def register(request):
+    form = UserCreationForm()
+    return render(request, 'accounts/register.html', {'form': form})
+
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    return redirect('index')
+
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
--- ceti_data_commons/ceti_data_commons/settings.py
+++ ceti_data_commons/ceti_data_commons/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
@@ -114,4 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'ceti_data_commons/static')
+]
