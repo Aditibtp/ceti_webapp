ec07c87eb97ea48330d07c69a1997e28703025ac
Carter Brown <carterbrwn2@gmail.com>
2019-08-19 14:32:51 -0400

    Added UserProfile to admin page

--- ceti_data_commons/accounts/admin.py
+++ ceti_data_commons/accounts/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserProfile
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'g_id')
+    list_display_links = ('user', 'g_id')
+    search_fields = ('user', 'g_id')
+    list_per_page = 25
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
